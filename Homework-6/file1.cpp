3ca2d3eb99dbfd1fdf57c03cd613a09bbe687ab2
samedov.l <samedov.leon@outlook.com>
2025-05-14 14:30:21 +0300

    Задание 1 в рамках ДЗ 6. Переписал весь код. Просьба игнорировать предыдущие коммиты по данному заданию.

--- Homework-6/file1.cpp
+++ Homework-6/file1.cpp
@@ -1,79 +1,94 @@
 //Task 1
 
-//#include <iostream>
-//#include <fstream>
-
-//int main() {
-    //std::ifstream inputFile("input.txt"); // input file
-    //if (!inputFile) {
-        //std::cerr << "An error occured during opening a file" << std::endl;
-        //return 1;
-    //}
-
-    //int rows, cols;
-    //inputFile >> rows >> cols;
-
-    //int size = rows * cols;
-    //int* array = new int[size];
-
-    //for (int i = 0; i < size; ++i) {
-        //inputFile >> array[i];
-    //}
-    //inputFile.close();
-
-    //std::cout << "Elements with even indeces: " << std::endl;
-    //for (int i = 0; i < size; ++i) {
-        //int row = i / cols;
-        //int col = i % cols;
-        //if ((row + col) % 2 == 0) {
-            //std::cout << array[i] << " ";
-        //}
-    //}
-    //std::cout << std::endl;
-
-    //delete[] array;
-
-    //return 0;
-//}
-
 #include <iostream>
 #include <fstream>
+#include <string>
 
+int main() 
+{
+    std::ifstream fin("inputexc1.txt");
+    if (!fin) 
+    {
+        std::cerr << "Error occured when opening a file.\n";
+        return 1;
+    }
 
-int main() {
-    std::ifstream inputFile("input2.txt"); // input file
-    if (!inputFile) {
-        std::cerr << "Error occured when opening a file" << std::endl;
+    // === Working with a one-dimensional array ===
+    std::string type1;
+    fin >> type1;
+    if (type1 != "1D") 
+    {
+        std::cerr << "Expected block 1D.\n";
+        return 1;
+    }
+
+    int n;
+    fin >> n;
+    if (n <= 0) 
+    {
+        std::cerr << "Incorrect array length.\n";
+        return 1;
+    }
+
+    int* arr1D = new int[n];
+    for (int i = 0; i < n; ++i) 
+    {
+        fin >> arr1D[i];
+    }
+
+    std::cout << "One-dimensional array - elements with even indices:\n";
+    for (int i = 0; i < n; i += 2) 
+    {
+        std::cout << arr1D[i] << " ";
+    }
+    std::cout << "\n";
+    delete[] arr1D;
+
+    // === Working with a two-dimensional array ===
+    std::string type2;
+    fin >> type2;
+    if (type2 != "2D") 
+    {
+        std::cerr << "Expected block 2D.\n";
         return 1;
     }
 
     int rows, cols;
-    inputFile >> rows >> cols;
+    fin >> rows >> cols;
+    if (rows <= 0 || cols <= 0) 
+    {
+        std::cerr << "Incorrect array dimensions.\n";
+        return 1;
+    }
 
-    int** array = new int* [rows];
-    for (int i = 0; i < rows; ++i) {
-        array[i] = new int[cols];
-        for (int j = 0; j < cols; ++j) {
-            inputFile >> array[i][j];
+    int** arr2D = new int* [rows];
+    for (int i = 0; i < rows; ++i) 
+    {
+        arr2D[i] = new int[cols];
+        for (int j = 0; j < cols; ++j) 
+        {
+            fin >> arr2D[i][j];
         }
     }
-    inputFile.close();
 
-    std::cout << "Elements with even indeces:" << std::endl;
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            if ((i + j) % 2 == 0) {
-                std::cout << array[i][j] << " ";
+    std::cout << "Two-dimensional array - elements with even sum of indices:\n";
+    for (int i = 0; i < rows; ++i) 
+    {
+        for (int j = 0; j < cols; ++j) 
+        {
+            if ((i + j) % 2 == 0) 
+            {
+                std::cout << arr2D[i][j] << " ";
             }
         }
     }
-    std::cout << std::endl;
+    std::cout << "\n";
 
-    // Releasing memory
-    for (int i = 0; i < rows; ++i) {
-        delete[] array[i];
+    for (int i = 0; i < rows; ++i) 
+    {
+        delete[] arr2D[i];
     }
-    delete[] array;
+    delete[] arr2D;
 
     return 0;
 }
